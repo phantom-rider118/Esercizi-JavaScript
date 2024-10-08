51cace39a000dde978c89ff59597715ebbcf406c
Clarissa De Benedittis <debenedittisclarissa04@gmail.com>
2024-10-08 19:51:17 +0200

    esercizi operatori logici

---
+++ Esercizio-gli-operatori-logici/index.js
@@ -0,0 +1,11 @@
+const isSunnyDay = true;
+const isAutumn = false;
+const itIsRaining = false;
+const isSummer = true;
+
+console.log(isSunnyDay && isSummer)
+console.log(isSunnyDay && isAutumn)
+console.log(isSummer || itIsRaining)
+console.log(itIsRaining || isAutumn)
+console.log(!isSummer)
+console.log(!itIsRaining)
---
+++ Esercizio12-gli-operatori-logici/index.js
@@ -0,0 +1,2 @@
+//Il corretto operatore logico in questo caso è l'OR, questo operatorere restituisce true se almeno una delle condizioni è vera
+console.log("testing" == "testing" || "Mario" == "Cool Guy")
