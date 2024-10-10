7ef722f81f71a9efac03d8f68d23ff02b779eb0c
Clarissa De Benedittis <debenedittisclarissa04@gmail.com>
2024-10-10 09:18:30 +0200

    esercizi operatore ternario

---
+++ Esercizio-operatore-ternario/index.js
@@ -0,0 +1,6 @@
+let isSunnyDay = true
+let isHomeworkCompleted = true
+
+isSunnyDay && isHomeworkCompleted
+? console.log("Jesse can go out to play")
+: console.log("Jesse stays at home");
---
+++ Esercizio15-operatore-ternario/index.js
@@ -0,0 +1,5 @@
+let age = 25
+
+age >= 18 
+? console.log("You can drive a car") 
+: console.log("You are too young to drive")
