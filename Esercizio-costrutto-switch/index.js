fa577398d704a8531eb42da50444cec6002401b6
Clarissa De Benedittis <debenedittisclarissa04@gmail.com>
2024-10-10 09:35:57 +0200

    esercizi costrutto switch

---
+++ Esercizio-costrutto-switch/index.js
@@ -0,0 +1,13 @@
+let favoriteFood = "pizza"
+
+switch(favoriteFood){
+    case "pizza":
+        console.log("You like PIZZA!")
+    break;
+    case "coconut":
+        console.log("You like COCONUT!")
+    break;
+    default:
+        console.log("You like something else!")
+    break;
+}
---
+++ Esercizio23-costrutto-switch/index.js
@@ -0,0 +1,17 @@
+let tariffa = "BB";
+let prezzo;
+
+switch(tariffa){
+    case "BB":
+        console.log("la tariffa per la camera d'albergo è 50£")
+    break;
+    case "HB":
+        console.log("la tariffa per la camera d'albergo è 80£")
+    break;
+    case "FB":
+        console.log("la tariffa per la camera d'albergo è 100£")
+    break;
+    default:
+        console.log("tariffa non valida")
+    break
+}
